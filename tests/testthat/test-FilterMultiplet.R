3963a8a35f65fc9dbd0cda341259fff12ea7ebf5
Sodiq Hameed <hameedsodiq71@gmail.com>
2025-09-09 13:23:01 +0100

    test added

---
+++ tests/testthat/test-FilterMultiplet.R
@@ -0,0 +1,40 @@
+
+
+test_that("FilterMultiplet filters multiplets correctly", {
+  # load example data included in package
+  data("int_multData", package = "ULM")
+  data("int_signature", package = "ULM")
+  library(ULM)
+  # compute scores
+  my_scores <- GetCellScores(
+    seurat_obj = int_multData[, 1:200],   # keep smaller subset for speed
+    signatures = int_signature,
+    assay = "RNA",
+    slot = "data"
+  )
+  
+  # assign cells
+  my_ass <- GetCellAssignments(score_data = my_scores)
+  
+  # add metadata
+  new_obj <- AddMetaObject(
+    seurat_obj = int_multData[, 1:200],
+    cell_class_df = my_ass
+  )
+  
+
+  # filter multiplets — expect a warning about removed cells
+  expect_warning(
+    my_mult_filt <- FilterMultiplet(seurat_obj = new_obj, minCells = 2, minFreq = 2),
+    regexp = "Removing .* cells missing data"
+  )
+  # expectations
+  expect_type(my_mult_filt, "list")
+  expect_named(my_mult_filt, c("multSummaryFilt", "multObjFilt"))
+  
+  expect_true(all(c("multipletType", "frequency") %in% colnames(my_mult_filt$multSummaryFilt)))
+  expect_s4_class(my_mult_filt$multObjFilt, "Seurat")
+  
+  # sanity check: filtered frequencies are >= minFreq
+  expect_true(all(my_mult_filt$multSummaryFilt$frequency >= 2))
+})
---
+++ tests/testthat/test-GetSignature.R
@@ -0,0 +1,28 @@
+test_that("GetSignature generates signatures correctly", {
+  # load an example scRNAseq data provided with the package
+  data("int_singData", package = "ULM")
+  
+  # run the function with a specified annotation column
+  expect_message(
+    int_sig <- GetSignature(
+      seurat_obj = int_singData[, 1:500], ###using 500 cells
+      ident_col = int_singData$Cell_Type,
+      n = 100,
+      p_val = 0.05
+    ),
+    regexp = "using the specified seurat ident"
+  )
+  
+  # structure tests
+  expect_s3_class(int_sig, "data.frame")
+  expect_true(all(c("source", "target", "mor") %in% colnames(int_sig)))
+  
+  # check the content of each column
+  expect_true(all(int_sig$mor == 1))
+  expect_true(all(!is.na(int_sig$source)))
+  expect_true(all(!is.na(int_sig$target)))
+  
+  # check that the number of genes in each cell type signature is ≤ n
+  cluster_counts <- table(int_sig$source)
+  expect_true(all(cluster_counts <= 100))
+})
